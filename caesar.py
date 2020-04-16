65d736a5e4d053b7c7b92255cd40c0670f4395fc
Maksimym200 <maksimym200@gmail.com>
2020-04-16 10:06:58 +0300

    Fix bad names and unsafe oppening.

--- caesar.py
+++ caesar.py
@@ -1,139 +1,108 @@
-from sys import stdout as _out
-from sys import stdin as _in
-from string import ascii_letters as _letters
-import json as _json
-from collections import Counter as _Counter
+from sys import stdout
+from sys import stdin
+from string import ascii_lowercase as letters
+import json
+from collections import Counter
 
-def _encode_symbol(_key, _s):
-    if not _s.isalpha():
-        return _s
-    elif _s.islower():
-        return chr((ord(_s) - ord('a') + _key) % 26 + ord('a'))
-    else:
-        return chr((ord(_s) - ord('A') + _key) % 26 + ord('A'))
-
-def stream_encode(_key, _input, _output):
-    while True:
-        try:
-            _s = _input.read(1)
-        except:
-            break
-        if _s == "":
-            break
-        _output.write(_encode_symbol(_key, _s))
-
-def stream_decode(_key, _input, _output):
-    while True:
-        try:
-            _s = _input.read(1)
-        except:
-            break
-        if _s == "":
-            break
-        _output.write(_encode_symbol(-_key, _s))
-
-def encode(_key, _input_name, _output_name):
-    _input = _in
-    _output = _out
-    if _input_name != None:
-        _input = open(_input_name, 'r')
-    if _output_name != None:
-        _output = open(_output_name, 'w')
-    stream_encode(int(_key), _input, _output)
-    if _input_name != None:
-        _input.close()
-    if _output_name != None:
-        _output.close()
-
-def decode(_key, _input_name, _output_name):
-    _input = _in
-    _output = _out
-    if _input_name != None:
-        _input = open(_input_name, 'r')
-    if _output_name != None:
-        _output = open(_output_name, 'w')
-    stream_decode(int(_key), _input, _output)
-    if _input_name != None:
-        _input.close()
-    if _output_name != None:
-        _output.close()
-
-def _get_frequency_data(_data):
-    _frequency_data = dict()
-    _data_size = sum(_data.values())
-    for _s in _data:
-        _frequency_data[_s] = (_data[_s] / _data_size)
-    for _i in _letters:
-        if not(_i in _frequency_data):
-            _frequency_data[_i] = 0
-    return _frequency_data
-
-def _get_key(_frequency_data, _model):
-    _key = 0
-    _similarity = 1
-    for _i in range(26):
-        _current_similarity = 0
-        for _s in _model:
-            if _encode_symbol(_i, _s) not in _frequency_data:
-                _current_similarity += _model[_s]
+def get_name_supporing_function(f):
+    def name_supporing_function(*args):
+        if args[0] == None:
+            if args[1] == None:
+                f(stdin, stdout, *args[2:])
             else:
-               _current_similarity += abs(_frequency_data[_encode_symbol(_i, _s)] - _model[_s])
-        if (_current_similarity <= _similarity):
-            _similarity = _current_similarity
-            _key = _i
-    return _key
+                with open(args[1], 'w') as output:
+                    f(stdin, output, *args[2:])
+        else:
+            if args[1] == None:
+                with open(args[0], 'r') as input:
+                    f(input, stdout, *args[2:])
+            else:
+                with open(args[0], 'r') as input:
+                    with open(args[1], 'w') as output:
+                        f(input, output, *args[2:])
+    return name_supporing_function
+        
 
-def stream_train(_input, _model):
-    _data = _Counter()
+def encode_symbol(key, s):
+    if not s.isalpha():
+        return s
+    elif s.islower():
+        return chr((ord(s) - ord('a') + key) % 26 + ord('a'))
+    else:
+        return chr((ord(s) - ord('A') + key) % 26 + ord('A'))
+
+def stream_encode(input, output, key):
     while True:
         try:
-            _s = _input.read(1)
+            s = input.read(1)
         except:
             break
-        if _s == "":
+        if s == "":
             break
-        if _s.isalpha():
-            _data += _Counter({_s})
-    _json.dump(_get_frequency_data(_data), _model)
+        output.write(encode_symbol(key, s))
 
-def stream_hack(_input, _output, _model):
-    _text = []
-    _text_data = _Counter()
+def stream_decode(input, output, key):
+    stream_encode(input, output, -key)
+
+def get_frequency_data(data):
+    frequency_data = dict()
+    data_size = sum(data.values())
+    for s in data:
+        frequency_data[s] = (data[s] / data_size)
+    for i in letters:
+        if not(i in frequency_data):
+            frequency_data[i] = 0
+    return frequency_data
+
+def get_key(frequency_data, model):
+    key = 0
+    similarity = 1
+    for i in range(26):
+        current_similarity = 0
+        for s in model:
+            current_similarity += abs(frequency_data[encode_symbol(i, s)] - model[s])
+        if (current_similarity <= similarity):
+            similarity = current_similarity
+            key = i
+    return key
+
+def stream_train(input, model):
+    data = Counter()
     while True:
         try:
-            _s = _input.read(1)
+            s = input.read(1)
         except:
             break
-        if _s == "":
+        if s == "":
             break
-        if _s.isalpha():
-            _text_data += _Counter({_s})
-        _text.append(_s)
-    _text_frequency_data = _get_frequency_data(_text_data)
-    _key = _get_key(_text_frequency_data, _model)
-    for i in range(len(_text)):
-        _output.write(_encode_symbol(-_key, _text[i]))
+        if s.isalpha():
+            data += Counter({s.lower()})
+    json.dump(get_frequency_data(data), model)
 
-def train(_input_name, _model_name):
-    _input = _in
-    if _input_name != None:
-        _input = open(_input_name, 'r')
-    _model = open(_model_name, 'w')
-    stream_train(_input, _model)
-    if _input_name != None:
-        _input.close()
+def stream_hack(input, output, model_name):
+    with open(model_name, "r") as model_encoded:
+        model = json.load(model_encoded)
+    text = []
+    text_data = Counter()
+    while True:
+        try:
+            s = input.read(1)
+        except:
+            break
+        if s == "":
+            break
+        if s.isalpha():
+            text_data += Counter({s.lower()})
+        text.append(s)
+    text_frequency_data = get_frequency_data(text_data)
+    key = get_key(text_frequency_data, model)
+    for i in range(len(text)):
+        output.write(encode_symbol(-key, text[i]))
 
-def hack(_input_name, _output_name, _model_name):
-    _input = _in
-    _output = _out
-    if _input_name != None:
-        _input = open(_input_name, 'r')
-    if _output_name != None:
-        _output = open(_output_name, 'w')
-    with open(_model_name, "r") as _model_encoded:
-        _model = _json.load(_model_encoded)
-    stream_hack(_input, _output, _model)
-    if _input_name != None:
-        _input.close()
-    if _output_name != None:
-        _output.close()
+encode = get_name_supporing_function(stream_encode)
+
+decode = get_name_supporing_function(stream_decode)
+
+train = get_name_supporing_function(stream_train)
+
+hack = get_name_supporing_function(stream_hack)
