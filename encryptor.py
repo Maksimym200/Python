428d85d429a8a169d502296b07708d2463ba117c
Maksimym200 <maksimym200@gmail.com>
2020-04-16 10:09:18 +0300

    Update for ciphers changes.

--- encryptor.py
+++ encryptor.py
@@ -1,34 +1,34 @@
-import argparse as _argparse
-import caesar as _caesar
-import vigenere as _vigenere
+import argparse
+import caesar
+import vigenere
 
-_parser = _argparse.ArgumentParser()
-_parser.add_argument("action", type = str, choices = ["encode", "decode", "train", "hack"])
-_parser.add_argument("--cipher", type = str, choices = ['caesar', 'vigenere'])
-_parser.add_argument("--key")
-_parser.add_argument("--input-file", type = str)
-_parser.add_argument("--output-file", type = str)
-_parser.add_argument("--text-file", type = str)
-_parser.add_argument("--model-file", type = str)
-args = _parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("action", type = str, choices = ["encode", "decode", "train", "hack"])
+parser.add_argument("--cipher", type = str, choices = ['caesar', 'vigenere'])
+parser.add_argument("--key")
+parser.add_argument("--input-file", type = str)
+parser.add_argument("--output-file", type = str)
+parser.add_argument("--text-file", type = str)
+parser.add_argument("--model-file", type = str)
+args = parser.parse_args()
 
 if args.action == "encode":
     if args.cipher == "caesar":
-        _caesar.encode(args.key, args.input_file, args.output_file)
+        caesar.encode(args.input_file, args.output_file, args.key)
     elif args.cipher == "vigenere":
-        _vigenere.encode(args.key, args.input_file, args.output_file)
+        vigenere.encode(args.input_file, args.output_file, args.key)
 
 if args.action == "decode":
     if args.cipher == "caesar":
-        _caesar.decode(args.key, args.input_file, args.output_file)
+        caesar.decode(args.input_file, args.output_file, args.key)
     if args.cipher == "vigenere":
-        _vigenere.decode(args.key, args.input_file, args.output_file)
+        vigenere.decode(args.input_file, args.output_file, args.key)
 
 if args.action == "train":
-    _caesar.train(args.text_file, args.model_file)
+    caesar.train(args.text_file, args.model_file)
 
 if args.action == "hack":
     if args.cipher == "caesar":
-        _caesar.hack(args.input_file, args.output_file, args.model_file)
+        caesar.hack(args.input_file, args.output_file, args.model_file)
     if args.cipher == "vigenere":
-        _vigenere.hack(args.input_file, args.output_file, args.model_file)
+        vigenere.hack(args.input_file, args.output_file, args.model_file)
