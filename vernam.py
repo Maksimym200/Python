22bbdaa08bd616308494efd18ac4d5ff59093414
Maksimym200 <maksimym200@gmail.com>
2020-04-27 22:59:00 +0300

    Stream changes

--- vernam.py
+++ vernam.py
@@ -1,51 +1,35 @@
-from sys import stdout
-from sys import stdin
-from string import ascii_lowercase as letters
-letters_indexes = {letters[i] : i for i in range(len(letters))}
+letters = []
+letters_indexes = dir()
 
-def get_name_supporing_function(f):
-    def name_supporing_function(*args):
-        if args[0] == None:
-            if args[1] == None:
-                f(stdin, stdout, *args[2:])
-            else:
-                with open(args[1], 'w') as output:
-                    f(stdin, output, *args[2:])
-        else:
-            if args[1] == None:
-                with open(args[0], 'r') as input:
-                    f(input, stdout, *args[2:])
-            else:
-                with open(args[0], 'r') as input:
-                    with open(args[1], 'w') as output:
-                        f(input, output, *args[2:])
-    return name_supporing_function
+def import_alphabet(_letters, _letters_indexes):
+    global letters
+    global letters_indexes
+    letters = _letters
+    letters_indexes = _letters_indexes  
 
 def encode_symbol(key_s, s):
     
-    if (not key_s.isalpha()) or (not s.isalpha())\
+    if (not key_s.lower() in letters_indexes) or (not s.lower() in letters_indexes)\
        or letters_indexes[s.lower()] ^ letters_indexes[key_s.lower()] >= len(letters):
         return s
-    elif s.islower():
+    elif s in letters_indexes:
         return letters[letters_indexes[s] ^ letters_indexes[key_s.lower()]]
     else:
         return letters[letters_indexes[s.lower()] ^ letters_indexes[key_s.lower()]].upper()
 
-def stream_encode(input, output, key):
-    index = 0
+def encode(input, output, key):
     while True:
-        try:
-            s = input.read(1)
-        except:
+        part = input.read(1024)
+        if part == "":
             break
-        if s == "":
+        encoded_str = []
+        index = 0
+        for s in part:
+            encoded_str.append(encode_symbol(key[index], s))
+            index = (index + 1) % len(key)
+        output.write("".join(encoded_str))
+        if len(part) < 1024:
             break
-        output.write(encode_symbol(key[index], s))
-        index = (index + 1) % len(key)
 
-def stream_decode(input, output, key):
-    stream_encode(input, output, key)
-
-encode = get_name_supporing_function(stream_encode)
-
-decode = get_name_supporing_function(stream_decode)
+def decode(input, output, key):
+    encode(input, output, key)
