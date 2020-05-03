f08055926a01ddf36bc1e73af2fc5caa5c1c3f3c
Maksimym200 <maksimym200@gmail.com>
2020-05-03 20:10:15 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,22 +1,10 @@
 import json
 from collections import Counter
-
-letters = []
-letters_indexes = dir()
-
-def import_alphabet(_letters, _letters_indexes):
-    global letters
-    global letters_indexes
-    letters = _letters
-    letters_indexes = _letters_indexes  
-
-def encode_symbol(key, s):
-    if s in letters_indexes:
-        return letters[(letters_indexes[s] + key) % len(letters)]
-    elif s.lower() in letters_indexes:
-        return letters[(letters_indexes[s.lower()] + key) % len(letters)].upper()
-    else:
-        return s
+from alphabet import letters
+from alphabet import indexes as letters_indexes
+from caesar import encode_symbol
+packet_size = 1024
+accuracy = 0.005
 
 def get_data_list(text, key):
     data_list = []
@@ -33,7 +21,7 @@ def get_frequency_data(data):
     for s in data:
         frequency_data[s] = (data[s] / data_size)
     for i in letters:
-        if not(i in frequency_data):
+        if i not in frequency_data:
             frequency_data[i] = 0
     return frequency_data
 
@@ -46,18 +34,18 @@ def index_of_coincidence(frequency_data):
 def get_correct_frequency_data_list(text, index):
     correct_frequency_data_list = []
     current_index = 0
-    for i in range(1, len(text) + 1, 1):
-        data_list = get_data_list(text, i)
+    for key_len in range(1, len(text) + 1, 1):
+        data_list = get_data_list(text, key_len)
         frequency_data_list = []
         average_index = 0
-        for j in range(i):
-            frequency_data_list.append(get_frequency_data(data_list[j]))
-            average_index += index_of_coincidence(frequency_data_list[j])
-        average_index /= i
+        for data in data_list:
+            frequency_data_list.append(get_frequency_data(data))
+            average_index += index_of_coincidence(frequency_data_list[-1])
+        average_index /= key_len
         if (average_index > current_index):
             correct_frequency_data_list = frequency_data_list
             current_index = average_index
-        if abs(average_index - index) < 0.005:
+        if abs(average_index - index) < accuracy:
             return frequency_data_list
     return correct_frequency_data_list
 
@@ -67,38 +55,40 @@ def get_key(text, model):
     frequency_data_list = get_correct_frequency_data_list(text, index)
     key_len = len(frequency_data_list)
     key = []
-    for j in range(key_len):
+    for frequency_data in frequency_data_list:
         key_symbol = 0
         similarity = 1
         for i in range(len(letters)):
             current_similarity = 0
             for s in model:
-                current_similarity += abs(frequency_data_list[j][encode_symbol(i, s)] - model[s])
+                current_similarity += abs(frequency_data[encode_symbol(i, s)] - model[s])
             if (current_similarity <= similarity):
                 similarity = current_similarity
                 key_symbol = i
         key.append(key_symbol)
+    print(key)
     return key
 
 
-def hack(input, output, model_name):
-    with open(model_name, "r") as model_encoded:
-        model = json.load(model_encoded)
+def hack(input, output, model_encoded):
+    model = json.load(model_encoded)
     text = list(input.read())
     key = get_key(text, model)
-    hacked_str = []
-    for i in range(len(text)):
-        hacked_str.append(encode_symbol(-key[i % len(key)], text[i]))
-        if len(hacked_str) == 1024:
-            output.write("".join(hacked_str))
-            hacked_str.clear()
-    output.write("".join(hacked_str))
+    index = 0
+    decoded_text = []
+    for s in text:
+        decoded_text.append(encode_symbol(-key[index], s))
+        index = (index + 1) % len(key)
+    output.write("".join(decoded_text))
 
 
 def train(input, model):
     data = Counter()
-    text = input.read()
-    for s in text:
-        if s.lower() in letters_indexes:
-            data += Counter({s.lower()})
+    while True:
+        packet = input.read(packet_size)
+        for s in packet:
+            if s.lower() in letters_indexes:
+                data += Counter({s.lower()})
+        if len(packet) < packet_size:
+            break
     json.dump(get_frequency_data(data), model)
