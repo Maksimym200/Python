01bbcbf9fd8b872559033655ee42f493f49c3749
Maksimym200 <maksimym200@gmail.com>
2020-05-03 20:07:50 +0300

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -1,36 +1,46 @@
 import argparse
-import caesar
-import vigenere
-import vernam
-import hack
+from ciphers import get_cipher
+import hack as _hack
 from sys import stdin
 from sys import stdout
+from contextlib import contextmanager
 
-from string import ascii_lowercase as letters
-letters_indexes = {letters[i] : i for i in range(len(letters))}
+@contextmanager
+def open_input(i):
+    if i == None:
+        yield stdin
+    else:
+        with open(i, "r") as input:
+            yield input
 
-def name_support(f):
-    def name_supporing_function(*args):
-        if args[0] == None:
-            if args[1] == None:
-                f(stdin, stdout, *args[2:])
-            else:
-                with open(args[1], 'w') as output:
-                    f(stdin, output, *args[2:])
-        else:
-            if args[1] == None:
-                with open(args[0], 'r') as input:
-                    f(input, stdout, *args[2:])
-            else:
-                with open(args[0], 'r') as input:
-                    with open(args[1], 'w') as output:
-                        f(input, output, *args[2:])
-    return name_supporing_function
+@contextmanager
+def open_output(i):
+    if i == None:
+        yield stdout
+    else:
+        with open(i, "w") as output:
+            yield output
 
-caesar.import_alphabet(letters, letters_indexes)
-vigenere.import_alphabet(letters, letters_indexes)
-vernam.import_alphabet(letters, letters_indexes)
-hack.import_alphabet(letters, letters_indexes)
+def encode(args):
+    with open_input(args.input_file) as input:
+        with open_output(args.output_file) as output:
+            get_cipher(args.cipher).encode(input, output, args.key)
+
+def decode(args):
+    with open_input(args.input_file) as input:
+        with open_output(args.output_file) as output:
+            get_cipher(args.cipher).decode(input, output, args.key)
+
+def train(args):
+    with open_input(args.text_file) as text:
+        with open_output(args.model_file) as model:
+            hack.train(text, model)
+
+def hack(args):
+    with open_input(args.input_file) as input:
+        with open_output(args.output_file) as output:
+            with open(args.model_file, 'r') as model:
+                _hack.hack(input, output, model)
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
@@ -39,46 +49,25 @@ encode_parser.add_argument("--cipher", type = str, choices = ['caesar', 'vigener
 encode_parser.add_argument("--key", required = True)
 encode_parser.add_argument("--input-file", type = str)
 encode_parser.add_argument("--output-file", type = str)
-encode_parser.set_defaults(action = "encode")
+encode_parser.set_defaults(action = encode)
 
 decode_parser = subparsers.add_parser('decode')
 decode_parser.add_argument("--cipher", type = str, choices = ['caesar', 'vigenere', 'vernam'], required = True)
 decode_parser.add_argument("--key", required = True)
 decode_parser.add_argument("--input-file", type = str)
 decode_parser.add_argument("--output-file", type = str)
-decode_parser.set_defaults(action = "decode")
+decode_parser.set_defaults(action = decode)
 
 train_parser = subparsers.add_parser('train')
 train_parser.add_argument("--model-file", type = str, required = True)
 train_parser.add_argument("--text-file", type = str)
-train_parser.set_defaults(action = "train")
+train_parser.set_defaults(action = train)
 
 hack_parser = subparsers.add_parser('hack')
 hack_parser.add_argument("--model-file", type = str, required = True)
 hack_parser.add_argument("--input-file", type = str)
 hack_parser.add_argument("--output-file", type = str)
-hack_parser.set_defaults(action = "hack")
+hack_parser.set_defaults(action = hack)
 
 args = parser.parse_args()
-
-if args.action == "encode":
-    if args.cipher == "caesar":
-        name_support(caesar.encode)(args.input_file, args.output_file, int(args.key))
-    elif args.cipher == "vigenere":
-        name_support(vigenere.encode)(args.input_file, args.output_file, str(args.key))
-    elif args.cipher == "vernam":
-        name_support(vernam.encode)(args.input_file, args.output_file, str(args.key))
-
-if args.action == "decode":
-    if args.cipher == "caesar":
-        name_support(caesar.decode)(args.input_file, args.output_file, int(args.key))
-    if args.cipher == "vigenere":
-        name_support(vigenere.decode)(args.input_file, args.output_file, str(args.key))
-    elif args.cipher == "vernam":
-        name_support(vernam.decode)(args.input_file, args.output_file, str(args.key))
-
-if args.action == "train":
-    name_support(hack.train)(args.text_file, args.model_file)
-
-if args.action == "hack":
-    name_support(hack.hack)(args.input_file, args.output_file, args.model_file)
+args.action(args)
