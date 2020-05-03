79cb9a5c972955f4d189eb99f707c802593566ca
Maksimym200 <maksimym200@gmail.com>
2020-05-03 20:08:36 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,28 +1,23 @@
-letters = []
-letters_indexes = dir()
-
-def import_alphabet(_letters, _letters_indexes):
-    global letters
-    global letters_indexes
-    letters = _letters
-    letters_indexes = _letters_indexes  
+from alphabet import letters
+from alphabet import indexes as letters_indexes
+packet_size = 1024
 
 def encode_symbol(key, s):
     if s in letters_indexes:
-        return letters[(letters_indexes[s] + key) % len(letters)]
+        return letters[(letters_indexes[s] + int(key)) % len(letters)]
     elif s.lower() in letters_indexes:
-        return letters[(letters_indexes[s.lower()] + key) % len(letters)].upper()
+        return letters[(letters_indexes[s.lower()] + int(key)) % len(letters)].upper()
     else:
         return s
 
 def encode(input, output, key):
-    while not input.closed:
-        part = input.read(1024)
+    while True:
+        packet = input.read(packet_size)
         encoded_str = []
-        for s in part:
+        for s in packet:
             encoded_str.append(encode_symbol(key, s))
         output.write("".join(encoded_str))
-        if len(part) < 1024:
+        if len(packet) < packet_size:
             break
 
 def decode(input, output, key):
