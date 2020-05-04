406578bc46e7719143183e54872d3bb63c3e94ab
Maksimym200 <maksimym200@gmail.com>
2020-05-04 18:22:31 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,24 +1,10 @@
 from alphabet import letters
 from alphabet import indexes as letters_indexes
-packet_size = 1024
-
-def encode_symbol(key, s):
-    if s in letters_indexes:
-        return letters[(letters_indexes[s] + int(key)) % len(letters)]
-    elif s.lower() in letters_indexes:
-        return letters[(letters_indexes[s.lower()] + int(key)) % len(letters)].upper()
-    else:
-        return s
+import vigenere
+encode_symbol = vigenere.encode_symbol
 
 def encode(input, output, key):
-    while True:
-        packet = input.read(packet_size)
-        encoded_str = []
-        for s in packet:
-            encoded_str.append(encode_symbol(key, s))
-        output.write("".join(encoded_str))
-        if len(packet) < packet_size:
-            break
+    vigenere.encode(input, output, letters[int(key) % len(letters)])
 
 def decode(input, output, key):
-    encode(input, output, -key)
+    vigenere.decode(input, output, letters[int(key) % len(letters)])
