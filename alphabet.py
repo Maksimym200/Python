e14119ee4d4bf2feec8443a86ec0bccfa3ebc89e
Maksimym200 <maksimym200@gmail.com>
2020-05-08 19:52:56 +0300

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,2 +1,6 @@
-from string import ascii_lowercase as letters
+from string import ascii_letters as english
+from string import digits as digits
+from string import punctuation as punctuation
+russian = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+letters = english + russian + digits + punctuation + ' \n'
 indexes = {letters[i] : i for i in range(len(letters))}
