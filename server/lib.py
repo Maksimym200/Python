60e3ef4d9c322589ca353646bd9616d14bd60003
Maksimym200 <maksimym200@gmail.com>
2020-05-18 10:24:00 +0300

    Update lib.py

--- server/lib.py
+++ server/lib.py
@@ -3,14 +3,17 @@ import vigenere
 from contextlib import contextmanager
 import hack as _hack
 
+
 @contextmanager
 def open_object(i, id):
     with open(f'{i}_{id}.txt', 'r') as object:
         yield json.load(object)
 
+
 def write_object(i, id, data):
     with open(f'{i}_{id}.txt', 'w') as object:
         json.dump(data, object)
+
 
 class Data:
     def __init__(self):
@@ -19,9 +22,10 @@ class Data:
             self.models = info[1]
             self.alphabets = info[0]          
 
+
     def add_model(self, input, description, ID):
         alphabet = self.get_alphabet(ID)
-        if alphabet == None:
+        if alphabet is None:
             return ' Error# Alphabet not found'
         if len(set(alphabet['letters']) & set(input)) == 0:
             return ' Error# Incorrect model. Model should contain symbols in alphabet'
@@ -31,6 +35,7 @@ class Data:
         with open('data.txt', 'w') as data:
             json.dump((self.alphabets, self.models), data)
         return index
+
 
     def add_alphabet(self, alphabet_letters, description):
         if len(set(alphabet_letters)) < len(alphabet_letters):
@@ -46,47 +51,55 @@ class Data:
             json.dump((self.alphabets, self.models), data)
         return index
 
-    def get_model_description(self, ID):
-        if not ID in self.models:
+
+    def get_object_description(self, ID, object):
+        objects = self.__getattribute__(f'{object}s')
+        if ID not in objects:
             return None
-        return self.models[ID]
+        return objects[ID]
+
+
+    def get_model_description(self, ID):
+        return self.get_object_description(ID, 'model')
+
 
     def get_alphabet_description(self, ID):
-        if not ID in self.alphabets:
+        return self.get_object_description(ID, 'alphabet')
+
+
+    def get_object(self, ID, object):
+        objects = self.__getattribute__(f'{object}s')
+        if ID not in objects:
             return None
-        return self.alphabets[ID]
+        with open_object(object, ID) as obj:
+            return obj
+
 
     def get_model(self, ID):
-        if not ID in self.models:
-            return None
-        with open_object('model', ID) as model:
-            return model
+        return self.get_object(ID, 'model')
+
 
     def get_alphabet(self, ID):
-        if not ID in self.alphabets:
-            return None
-        with open_object('alphabet', ID) as alphabet:
-            return alphabet
+        return self.get_object(ID, 'alphabet')
 
-    def encode(self, input, key, ID):
+
+    def encode(self, input, key, ID, shift = 'encode'):
         alphabet = self.get_alphabet(ID)
-        if alphabet == None:
+        if alphabet is None:
             return ' #Error Alphabet not found'
         if not set(key) <= set(alphabet['letters']):
             return ' #Error Alphabet don\'t support some of Key symbols'
-        return vigenere.encode(input, key, alphabet)
+        f = vigenere.__getattribute__(shift)
+        return f(input, key, alphabet)
+
 
     def decode(self, input, key, ID):
-        alphabet = self.get_alphabet(ID)
-        if alphabet == None:
-            return ' #Error Alphabet not found'
-        if not set(key) <= set(alphabet['letters']):
-            return ' #Error Alphabet don\'t support some of Key symbols'
-        return vigenere.decode(input, key, alphabet)
+        return self.encode(input, key, ID, 'decode')
+
 
     def hack(self, input, ID):
         model = self.get_model(ID)
-        if model == None:
+        if model is None:
             return ' #Error Model not found'
         alphabet = self.get_alphabet(self.get_model_description(ID)[0])
         return _hack.hack(input, model, alphabet)
