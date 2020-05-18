24293e8a0074b16e74cda32fa151a53f641e1905
Maksimym200 <maksimym200@gmail.com>
2020-05-18 14:36:59 +0300

    Update client.py

--- client/client.py
+++ client/client.py
@@ -13,6 +13,7 @@ def open_input(i):
         with open(i, 'r') as input:
             yield input
 
+
 @contextmanager
 def open_output(o):
     if o == '':
@@ -22,19 +23,13 @@ def open_output(o):
         with open(o, 'w') as output:
             yield output
 
+
 def create_main_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='localhost')
     parser.add_argument('--port', default=8888, type=int)
-
     return parser
 
-def check_type(type, obj):
-    try:
-       type(obj)
-    except ValueError:
-        return False
-    return True
 
 def request_command(main_args, method, command, args):
     address = f'http://{main_args.host}:{main_args.port}/{command}'
@@ -43,36 +38,51 @@ def request_command(main_args, method, command, args):
     if method == 'POST':
         return requests.post(address, params = args).text
 
+
 def input_command(main_args, input, method, command, args):
     with open_input(input) as i:
         args['input'] = i.read()
         return request_command(main_args, method, command, args)
 
+
+def advanced_input(message, t):
+    i = input(message)
+    try:
+       return t(i)
+    except TypeError:
+        return None
+
+
 def info(main_args, object):
-    ID = input(f' Print {object} ID:  ')
-    if not check_type(int, ID):
-        print(' Error# Incorrect ID')
-        return
-    ID = int(ID)
-    help = request_command(main_args, 'GET', f'get_{object}_information', dict(ID = ID))
-    return f' ID {ID}: {help}'
+    if object not in {'model', 'alphabet'}:
+        return f' Error# Unknown object \'{object}\'. Type \'help\' to get help'
+    ID = advanced_input(f' Print {object} ID:  ', int)
+    if ID is None:
+        return ' Error# Incorrect ID'
+    information = request_command(main_args, 'GET', f'get_{object}_information', dict(ID = ID))
+    return f' ID {ID}: {information}'
 
-def encode(main_args, shift = 'encode'):
-    ID = input(f' Print alphabet ID:  ')
-    if not check_type(int, ID):
-        print(' Error# Incorrect ID')
-        return
-    ID = int(ID)
 
-    key = input(f' Print key:  ')
-    if not check_type(str, ID):
-        print(' Error# Incorrect Key')
-        return
-    key = str(key)
+def encrypt(main_args, action):
+    if action == 'hack':
+        ID = advanced_input(f' Print model ID:  ', int)
+    else:
+        ID = advanced_input(f' Print alphabet ID:  ', int)
 
-    i = input(f' Print name of the file to {shift} (Tap Enter to read from console):  ')
+    if ID is None:
+        return ' Error# Incorrect ID'
+
+    if action != 'hack':
+        key = advanced_input(f' Print key:  ', str)
+        if key is None:
+            return ' Error# Incorrect key'
+
+    i = input(f' Print name of the file to {action} (Tap Enter to read from console):  ')
     try:
-        result = input_command(main_args, i, 'GET', shift, dict(ID = ID, key = key))
+        if action != 'hack':
+            result = input_command(main_args, i, 'GET', action, dict(ID = ID, key = key))
+        else:
+            result = input_command(main_args, i, 'GET', action, dict(ID = ID))
     except FileNotFoundError:
         print(' Error# Input file not found')
 
@@ -83,24 +93,6 @@ def encode(main_args, shift = 'encode'):
     except FileNotFoundError:
         print(' Error# Output file not found')
 
-def hack(main_args):
-    ID = input(f' Print model ID:  ')
-    try:
-        ID = int(ID)
-    except ValueError:
-        print(' Error# Incorrect ID')
-        return
-    i = input(f' Print name of the file to hack (Tap Enter to read from console):  ')
-    try:
-        result = input_command(main_args, i, 'GET', 'hack', dict(ID = ID))
-    except FileNotFoundError:
-        print(' Error# Input file not found')
-    o = input(f' Print name of the file to save the result (Tap Enter to write to console):  ')
-    try:
-        with open_output(o) as out:
-            out.write(result)
-    except FileNotFoundError:
-        print(' Error# Output file not found')
 
 def add_alphabet(main_args):
     desc = str(input(' Print description of the alphabet:  '))
@@ -113,12 +105,12 @@ def add_alphabet(main_args):
         return ' Error# Incorrect alphabet. Alphabet should not contain same symbols'
     return f' Added with ID: {result}'
 
+
 def add_model(main_args):
-    ID = input(' Print ID of the model\'s alphabet:  ')
-    if not check_type(int, ID):
-        print(' Error# Incorrect ID')
-        return
-    ID = int(ID)
+    ID = advanced_input(' Print ID of the model\'s alphabet:  ', int)
+    if ID is None:
+        return ' Error# Incorrect ID'
+
     desc = str(input(' Print description of the model:  '))
     i = input(' Print name of the model file (Tap Enter to read from console):  ')
     try:
@@ -129,14 +121,25 @@ def add_model(main_args):
         return ' Error# Alphabet not found'
     return f' Added with ID: {result}'
 
+
+def add(main_args, object):
+    if object == 'alphabet':
+        return add_alphabet(main_args)
+    elif object == 'model':
+        return add_model(main_args)
+    else:
+        return f' Error# Unknown object \'{object}\'. Type \'help\' to get help'
+
+
 def help():
     print(' Available commands: ')
     print(' - info {alphabet / model} : get alphabet or model information')
     print(' - add {alphabet / model} : add alphabet or model')
     print(' - encode : encode text with vigenere cipher')
-    print(' - decode : decode text with vigenere cipher')
+    print(' - decode : decode input with vigenere cipher')
     print(' - hack : hack encoded text')
     print(' - exit : exit the program')
+
 
 def try_exit():
     while True:
@@ -151,35 +154,29 @@ def try_exit():
         else:
             print(' Error# Incorrect input')
 
+
 def main():
     main_parser = create_main_parser()
     main_args = main_parser.parse_args()
     while True:
         try:
-            command = input('\n Enter command:  ')
-            if command == 'info alphabet':
-                print(info(main_args, 'alphabet'))
-            elif command == 'info model':
-                print(info(main_args, 'model'))
-            elif command == 'add alphabet':
-                print(add_alphabet(main_args))
-            elif command == 'add model':
-                print(add_model(main_args))
-            elif command == 'encode':
-                encode(main_args)
-            elif command == 'decode':
-                encode(main_args, 'decode')
-            elif command == 'hack':
-                hack(main_args)
-            elif command == 'help':
+            command = input('\n Enter command:  ').split(' ')
+            if command[0] == 'info' and len(command) > 1:
+                print(info(main_args, command[1]))
+            elif command[0] == 'add' and len(command) > 1:
+                print(add(main_args, command[1]))
+            elif command[0] in {'encode', 'decode', 'hack'}:
+                encrypt(main_args, command[0])
+            elif command[0] == 'help':
                 help()
-            elif command == 'exit':
+            elif command[0] == 'exit':
                 try_exit()
             else:
-                print(f' Unknown command \'{command}\'. Type \'help\' to get help')
+                print(f' Error# Unknown command \'{command[0]}\'. Type \'help\' to get help')
         except KeyboardInterrupt:
-            print('')
+            print()
             try_exit()
+
 
 if __name__ == '__main__':
     try:
