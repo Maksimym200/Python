2dfacadb64b5c2465f6db82acd33ba1ed6a97756
Maksimym200 <maksimym200@gmail.com>
2020-05-23 18:53:39 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,59 +1,65 @@
 import flask
 import lib
 import json
+import argparse
+from strings import methods, objects, messages
 
 
 app = flask.Flask('encryptor')
 Data = lib.Data()
 
 
-@app.route('/encode', methods=['GET'])
+@app.route(methods.encode(), methods=['GET'])
 def encode():
     args = flask.request.args
     return Data.encode(str(args['input']), str(args['key']), str(args['ID']))
 
 
-@app.route('/decode', methods=['GET'])
+@app.route(methods.decode(), methods=['GET'])
 def decode():
     args = flask.request.args
     return Data.decode(str(args['input']), str(args['key']), str(args['ID']))
 
 
-@app.route('/hack', methods=['GET'])
+@app.route(methods.hack(), methods=['GET'])
 def hack():
     args = flask.request.args
     return Data.hack(str(args['input']), str(args['ID']))
 
 
-@app.route('/add_alphabet', methods=['POST'])
+@app.route(methods.add(objects.alphabet()), methods=['POST'])
 def add_alphabet():
     args = flask.request.args
     return str(Data.add_alphabet(str(args['input']), str(args['description'])))
 
 
-@app.route('/add_model', methods=['POST'])
+@app.route(methods.add(objects.model()), methods=['POST'])
 def add_model():
     args = flask.request.args
     return str(Data.add_model(str(args['input']), str(args['description']), str(args['ID'])))
 
 
-@app.route('/get_alphabet_information', methods=['GET'])
-def get_alphabet_information():
+@app.route(methods.getinfo(objects.alphabet()), methods=['GET'])
+def getinfo_alphabet():
     ID = str(flask.request.args['ID'])
     alphabet_desc = Data.get_alphabet_description(ID)
     if not alphabet_desc:
-        return ' No information found'
+        return messages.no_info()
     return alphabet_desc
 
 
-@app.route('/get_model_information', methods=['GET'])
-def get_model_information():
+@app.route(methods.getinfo(objects.model()), methods=['GET'])
+def getinfo_model():
     ID = str(flask.request.args['ID'])
     model_desc = Data.get_model_description(ID)
     if model_desc is None:
-        return ' No information found'
-    return f'Model alphabet ID: {model_desc[0]}\n {model_desc[1]}'
+        return messages.no_info()
+    return messages.model_info(model_desc[0], model_desc[1])
 
 
 if __name__ == '__main__':
-    app.run('::', port=8888)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='::')
+    parser.add_argument('--port', default=8888, type=int)
+    args = parser.parse_args()
+    app.run(args.host, args.port)
