e0c522ff511c8480868d754038cee66cbdecd7b0
Maksimym200 <maksimym200@gmail.com>
2020-05-23 21:04:37 +0300

    Update lib.py

--- server/lib.py
+++ server/lib.py
@@ -2,7 +2,7 @@ import json
 import vigenere
 from contextlib import contextmanager
 import hack as _hack
-from strings import errors, objects
+from constants import errors, objects
 
 
 data_file = 'data.txt'
@@ -29,26 +29,26 @@ class Data:
     def add_model(self, input, description, ID):
         alphabet = self.get_alphabet(ID)
         if not alphabet:
-            return errors.no_alphabet()
+            return errors.no_alphabet
         if not set(alphabet['letters']) & set(input):
-            return errors.incorrect_model() + '\n' + errors.empty_model()
+            return errors.incorrect_model + '\n' + errors.empty_model
         index = str(len(self.models))
         self.models[index] = (ID, description)
-        write_object(objects.model(), index, _hack.train(input, alphabet))
+        write_object(objects.model, index, _hack.train(input, alphabet))
         with open(data_file, 'w') as data:
             json.dump((self.alphabets, self.models), data)
         return index
 
     def add_alphabet(self, alphabet_letters, description):
         if len(set(alphabet_letters)) < len(alphabet_letters):
-            return errors.incorrect_alphabet() + '\n' + errors.duplicate_alphabet()
+            return errors.incorrect_alphabet + '\n' + errors.duplicate_alphabet
         if not alphabet_letters:
-            return errors.incorrect_alphabet() + '\n' + errors.empty_alphabet()
+            return errors.incorrect_alphabet + '\n' + errors.empty_alphabet
         index = str(len(self.alphabets))
         self.alphabets[index] = description
         alphabet = {'letters' : alphabet_letters, \
                     'indexes' : {alphabet_letters[i] : i for i in range(len(alphabet_letters))}}
-        write_object(objects.alphabet(), index, alphabet)
+        write_object(objects.alphabet, index, alphabet)
         with open(data_file, 'w') as data:
             json.dump((self.alphabets, self.models), data)
         return index
@@ -60,10 +60,10 @@ class Data:
         return objects[ID]
 
     def get_model_description(self, ID):
-        return self.get_object_description(ID, objects.model())
+        return self.get_object_description(ID, objects.model)
 
     def get_alphabet_description(self, ID):
-        return self.get_object_description(ID, objects.alphabet())
+        return self.get_object_description(ID, objects.alphabet)
 
     def get_object(self, ID, obj):
         objects = self.__getattribute__(f'{obj}s')
@@ -73,26 +73,26 @@ class Data:
             return obj
 
     def get_model(self, ID):
-        return self.get_object(ID, objects.model())
+        return self.get_object(ID, objects.model)
 
     def get_alphabet(self, ID):
-        return self.get_object(ID, objects.alphabet())
+        return self.get_object(ID, objects.alphabet)
 
-    def encode(self, input, key, ID, shift = objects.encode()):
+    def encode(self, input, key, ID, shift = objects.encode):
         alphabet = self.get_alphabet(ID)
         if not alphabet:
-            return errors.no_alphabet()
+            return errors.no_alphabet
         if not set(key) <= set(alphabet['letters']):
-            return errors.incorrect_key()
+            return errors.incorrect_key
         f = getattr(vigenere, shift)
         return f(input, key, alphabet)
 
     def decode(self, input, key, ID):
-        return self.encode(input, key, ID, objects.decode())
+        return self.encode(input, key, ID, objects.decode)
 
     def hack(self, input, ID):
         model = self.get_model(ID)
         if not model:
-            return errors.no_model()
+            return errors.no_model
         alphabet = self.get_alphabet(self.get_model_description(ID)[0])
         return _hack.hack(input, model, alphabet)
