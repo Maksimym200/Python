d43e6ee5d4a5882da484ba74c747adcece1d6f4b
Maksimym200 <maksimym200@gmail.com>
2020-05-24 18:20:18 +0300

    Update vernam.py

--- vernam.py
+++ vernam.py
@@ -1,16 +1,9 @@
 from vernam_alphabet import letters
 from vernam_alphabet import indexes as letters_indexes
-from math import log2, floor
-encodable_size = 2 ** floor(log2(len(letters)))
 
 
 def encode_symbol(key_s, s):
-    
-    if (not key_s in letters_indexes) or (not s in letters_indexes)\
-       or letters_indexes[s] >= encodable_size or letters_indexes[key_s] >= encodable_size:
-        return s
-    else:
-        return letters[letters_indexes[s] ^ letters_indexes[key_s]]
+    return letters[letters_indexes[s] ^ letters_indexes[key_s]]
 
 def encode(input, output, key):
     while True:
